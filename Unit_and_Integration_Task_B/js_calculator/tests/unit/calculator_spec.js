61c6e0f4e1d927c24e13c23082ce6d6f62eb3df1
Rob Armitage <robert.c.armitage@gmail.com>
2019-12-04 04:07:57 +0000

    add unit tests for numberClick, operatorClick and clearClick; unit testing complete

--- Unit_and_Integration_Task_B/js_calculator/tests/unit/calculator_spec.js
+++ Unit_and_Integration_Task_B/js_calculator/tests/unit/calculator_spec.js
@@ -11,7 +11,7 @@ describe('calculator', function () {
     calculator.previousTotal = 4
     calculator.add(1)
     const actual = calculator.runningTotal
-    assert.strictEqual(actual, 5)
+    assert.equal(actual, 5)
   })
 
   // subtract
@@ -19,7 +19,7 @@ describe('calculator', function () {
     calculator.previousTotal = 7
     calculator.subtract(4)
     const actual = calculator.runningTotal
-    assert.strictEqual(actual, 3)
+    assert.equal(actual, 3)
   })
 
   // multiply
@@ -27,7 +27,7 @@ describe('calculator', function () {
     calculator.previousTotal = 3
     calculator.multiply(5)
     const actual = calculator.runningTotal
-    assert.strictEqual(actual, 15)
+    assert.equal(actual, 15)
   })
 
   // divide
@@ -35,7 +35,39 @@ describe('calculator', function () {
     calculator.previousTotal = 21
     calculator.divide(7)
     const actual = calculator.runningTotal
-    assert.strictEqual(actual, 3)
+    assert.equal(actual, 3)
+  })
+
+  // nunmberClick
+  it('should concatenate multiple button clicks', function(){
+    calculator.numberClick(5)
+    calculator.numberClick(6)
+    const actual = calculator.runningTotal
+    assert.equal(actual, 56)
+  })
+
+  // operatorClick
+  it('should chain multiple operations together', function(){
+    calculator.numberClick(3)
+    calculator.operatorClick('+')
+    calculator.numberClick(3)
+    calculator.operatorClick('*')
+    calculator.numberClick(2)
+    calculator.operatorClick('=')
+    const actual = calculator.runningTotal
+    assert.equal(actual, 12)
+  })
+
+  // clearClick
+  it('should clear the running total without affecting the calculation', function(){
+    calculator.numberClick(3)
+    calculator.operatorClick('+')
+    calculator.numberClick(3)
+    calculator.operatorClick('*')
+    calculator.numberClick(2)
+    calculator.clearClick()
+    const actual = calculator.previousTotal
+    assert.equal(actual, 6)
   })
 
 });
