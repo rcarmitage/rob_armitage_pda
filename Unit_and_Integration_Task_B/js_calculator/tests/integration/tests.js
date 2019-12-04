ce9319d51fc243eb047a446535a7653a0506c435
Rob Armitage <robert.c.armitage@gmail.com>
2019-12-04 04:53:53 +0000

    add integration testing tasks 1-4

--- Unit_and_Integration_Task_B/js_calculator/tests/integration/tests.js
+++ Unit_and_Integration_Task_B/js_calculator/tests/integration/tests.js
@@ -9,11 +9,88 @@ describe('calculator functionality', function() {
     browser.get('http://localhost:3000');
   });
 
-  // write integration tests here in the form of "it should do something..."
-  it('should have working number buttons', function(){
+  // 1. Do the number buttons update the display of the running total?
+  it('should have number buttons which update the display of the running total', function(){
     running_total = element(by.css('#running_total'))
     element(by.css('#number2')).click();
     expect(running_total.getAttribute('value')).to.eventually.equal('2')
+  })
+
+  // 2. Do the arithmetical operations update the display with the result of the operation?
+  it('should update the display when operators are clicked', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('4')
+  })
+
+  // 3. Can multiple operations be chained together?
+  it('should chain multiple operations', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('1')
+  })
+
+  // 4. Is the output as expected for a range of numbers - positive
+  it('should show positive numbers', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number9')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('18')
+  })
+
+  // 4. Is the output as expected for a range of numbers - negative
+  it('should show negative numbers', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number3')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number6')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('-3')
+  })
+
+  // 4. Is the output as expected for a range of numbers - decimal
+  it('should show numbers with decimals', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number3')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('1.5')
+  })
+
+  // 4. Is the output as expected for a range of numbers - very large
+  it('should show very large numbers', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number1')).click();
+    element(by.css('#number1')).click();
+    element(by.css('#number1')).click();
+    element(by.css('#number1')).click();
+    element(by.css('#number1')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('108898911')
+  })
+
+  // 5. What does the code do in exceptional circumstances? - divide by zero
+  it('should show an error message when dividing by zero', function(){
+    running_total = element(by.css('#running_total'))
+  
+    expect(running_total.getAttribute('value')).to.eventually.equal('Error - cannot divide by 0')
   })
 
 });
